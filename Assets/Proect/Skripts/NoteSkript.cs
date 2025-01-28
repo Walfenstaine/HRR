aaebd104917ec28e47a3cfb4d9ba956bcd25ca6d
Walfwnstaine <121248230+Walfenstaine@users.noreply.github.com>
2025-01-28 19:26:24 +0300

    444

---
+++ Assets/Proect/Skripts/NoteSkript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteSkript : MonoBehaviour {
+    public Data data;
+    [SerializeField] private Language language;
+    public AudioClip clip;
+    public void Rider()
+    {
+        SoundPlayer.regit.Play(clip,1);
+        if (data.language == "ru")
+        {
+            NoteRider.regit.Noting(language.ru);
+        }
+        else
+        {
+            NoteRider.regit.Noting(language.en);
+        }
+    }
+}
